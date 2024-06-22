b7aa394626520a03e6b9bfdb3463b6324791c241
MAC <mac@Owens.local>
2024-06-22 20:36:47 +0200

    adding files

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+#Define the number
+number1 = 10
+number2 = 5
+#perform arithmetic operations
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+# print the result
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"substraction of {number1} and {number2} is {difference_result}")
+print(f"Multiplication of {number1} and {number2} is {product_result}")
---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,7 @@
+current_age = int(input("How old are you? "))
+
+# Calculate the future age in 2050
+future_age = current_age + (2050 - 2023)
+
+# Print the result
+print(f"In 2050, you will be {future_age} years old.")
